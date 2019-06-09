63d16a3d8167187a714165500ecb33889c1f6a0f
Yuri Becker <becker.yr@gmail.com>
2019-06-09 18:46:54 -0300

    Add excs de exemplo

---
+++ trabalho/main.asm
@@ -0,0 +1,10 @@
+# Autor: Yuri Becker e Daniel Libaroni
+# Descrição: Trabalho da disciplina de Organização de Computadores 
+
+.text                               # segmento de texto (código)
+.globl main                         
+
+main:
+
+
+.data
